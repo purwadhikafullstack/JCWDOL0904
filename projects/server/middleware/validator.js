257aa419ce911cb6eb4a548021904ed8ef3f2097
anggaihza <angga.asep98@gmail.com>
2023-06-05 12:34:17 +0700

    validator

--- projects/server/middleware/validator.js
+++ projects/server/middleware/validator.js
@@ -1,11 +1,11 @@
 const access = process.env.KEY_ACCESS
 
 module.exports = (req, res, next) => {
-        console.log("aaaa", req.headers.key);
-        console.log("bbbb", access);
-        if (req.headers.key == access) {
-                // res.send("Suksess akses")
-                return next()
-        }
-        res.status(500).send({ message: "Authorization failed" })
+  console.log("aaaa", req.headers.key);
+  console.log("bbbb", access);
+  if (req.headers.key == access) {
+    // res.send("Suksess akses")
+    return next()
+  }
+  res.status(500).send({ message: "Authorization failed" })
 } 
