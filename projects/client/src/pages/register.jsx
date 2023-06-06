1341cd046958f002684c2180c3b2b88cccef18ed
ADI NUGROHO <alexiusnicholas@rocketmail.com>
2023-06-06 21:28:09 +0700

    register frontend added

---
+++ projects/client/src/pages/register.jsx
@@ -0,0 +1,108 @@
+import { React, useState, useRef, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+import { api } from "../API/api";
+
+const url = "/auth/verification";
+
+export const Verification = () => {
+  let navigate = useNavigate();
+
+  let passwordOne = useRef();
+  let confirmPassword = useRef();
+
+  let { token } = useParams();
+  //   console.log(token);
+
+  //useRef
+  // <Input type="file" display="hide" />
+  // <div ref={useRef} />
+
+  const TokenVerification = async () => {
+    // console.log(passwordOne, confirmPassword);
+    try {
+      if (token) {
+        console.log(passwordOne.current.value);
+        let response = await api.post(
+          url,
+          { password: passwordOne.current.value },
+          {
+            headers: {
+              authorization: `Bearer ${token}`,
+            },
+          }
+        );
+      }
+      //   console.log(response);
+      setTimeout(() => {
+        navigate("/");
+      }, 1000);
+    } catch (error) {
+      console.log(error);
+      return { response: error.response.data.message };
+    }
+  };
+
+  //   useState;
+
+  return (
+    <section class="bg-gray-50 dark:bg-gray-900">
+      <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Verification
+            </h1>
+            <div>
+              <label
+                for="email"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Password"
+                required=""
+                ref={passwordOne}
+              />
+            </div>
+            <div>
+              <label
+                for="email"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Confirm Password
+              </label>
+              <input
+                type="password"
+                name="confirmPassword"
+                id="confirmPassword"
+                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Confirm Password"
+                required=""
+                ref={confirmPassword}
+              />
+            </div>
+            <div class="flex items-start"></div>
+            <button
+              type="submit"
+              class="w-full text-black bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              onClick={() =>
+                TokenVerification(
+                  passwordOne.current.value,
+                  confirmPassword.current.value
+                )
+              }
+            >
+              OK
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
