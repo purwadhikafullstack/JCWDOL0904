706636b26d1179c068ef495baa4f8f8933efaca5
Znrrrf <123227576+Znrrrf@users.noreply.github.com>
2023-06-22 22:48:25 +0700

    add auto and manual mutation extra manage warehouse

--- projects/client/src/components/admin/Sidebar.jsx
+++ projects/client/src/components/admin/Sidebar.jsx
@@ -32,8 +32,7 @@ import { Button } from "@chakra-ui/react";
 const navigation = [
   {
     name: "Dashboard",
-    name: "Dashboard",
-    href: "/manage-warehouse",
+    href: "/order",
     icon: HomeIcon,
     current: true,
   },
@@ -44,16 +43,16 @@ const navigation = [
     current: true,
   },
   {
-    name: "User",
-    href: "/test",
+    name: "Mutation",
+    href: "/mutation-list",
     icon: UsersIcon,
     current: false,
   },
   {
-    name: "Mutation List",
-    href: "/mutation-list",
-    icon: FolderIcon,
-    current: false,
+    name: "Warehouse",
+    href: "/manage-warehouse",
+    icon: HomeIcon,
+    current: true,
   },
   {
     name: "Category",
@@ -61,8 +60,8 @@ const navigation = [
     icon: CalendarIcon,
     current: false,
   },
-  { name: "Documents", href: "/", icon: InboxIcon, current: false },
-  { name: "Reports", href: "/", icon: ChartBarIcon, current: false },
+  { name: "Product", href: "#", icon: InboxIcon, current: false },
+  { name: "Reports", href: "#", icon: ChartBarIcon, current: false },
 ];
 const userNavigation = [
   { name: "Your Profile", href: "#" },
@@ -323,7 +322,7 @@ export default function Sidebar() {
             </div>
           </div>
 
-          <main className="flex-1">
+          {/* <main className="flex-1">
             <div className="py-6">
               <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
                 <h1 className="text-2xl font-semibold text-gray-900">
@@ -336,7 +335,7 @@ export default function Sidebar() {
                 </div>
               </div>
             </div>
-          </main>
+          </main> */}
         </div>
       </div>
     </>
